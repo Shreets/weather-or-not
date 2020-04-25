9554a2e2800e2b1f64e14b92fde73998d27dcf10
Shreets <shreeti.019@gmai;.com>
2020-04-25 20:03:25 +0545

    Updates

---
+++ precache-manifest.a35f47cf4efbcefdccc4a8cdc0ba08be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8004a97d43a356e815321f5781d27d",
+    "url": "/weather-or-not/index.html"
+  },
+  {
+    "revision": "134ed91db9f4c23b2599",
+    "url": "/weather-or-not/static/css/main.71746eb9.chunk.css"
+  },
+  {
+    "revision": "431f4d54a7c18af9d1f5",
+    "url": "/weather-or-not/static/js/2.5f1d6f8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-or-not/static/js/2.5f1d6f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134ed91db9f4c23b2599",
+    "url": "/weather-or-not/static/js/main.be77b460.chunk.js"
+  },
+  {
+    "revision": "aeba6b9285729830a24a",
+    "url": "/weather-or-not/static/js/runtime-main.53599e5e.js"
+  },
+  {
+    "revision": "14ed407bb094fc6e9ef549c6ed763253",
+    "url": "/weather-or-not/static/media/cold.14ed407b.jpg"
+  },
+  {
+    "revision": "812e69c30d8418510242c5cd41e80646",
+    "url": "/weather-or-not/static/media/warm.812e69c3.jpg"
+  }
+]);
